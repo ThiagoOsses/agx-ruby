fa191eda27f42240fd68ae01908514863c20c590
Bryce Johnston <johnstonbrc@gmail.com>
2016-12-02 11:56:22 -0600

    Add httplog for debugging

--- agx.gemspec
+++ agx.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "oj", "~> 2.17.1"
+  spec.add_dependency "httplog", "~> 0.3.3"
 
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/agx.rb
+++ lib/agx.rb
@@ -1,5 +1,6 @@
 require 'oauth2'
 require 'oj'
+require 'httplog'
 
 require "agx/version"
 require "agx/error"
@@ -7,4 +8,17 @@ require "agx/content"
 require "agx/sync"
 
 module Agx
+  HttpLog.options[:logger]        = Logger.new($stdout)
+  HttpLog.options[:severity]      = Logger::Severity::DEBUG
+  HttpLog.options[:log_connect]   = true
+  HttpLog.options[:log_request]   = true
+  HttpLog.options[:log_headers]   = true
+  HttpLog.options[:log_data]      = true
+  HttpLog.options[:log_status]    = true
+  HttpLog.options[:log_response]  = true
+  HttpLog.options[:log_benchmark] = true
+  HttpLog.options[:compact_log]   = false
+  HttpLog.options[:color]         = true
+  HttpLog.options[:url_whitelist_pattern] = /.*/
+  HttpLog.options[:url_blacklist_pattern] = nil
 end
